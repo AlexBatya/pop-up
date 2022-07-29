defa2b03a5cd5b8a0a6af6b6a7f54db866f3bbf9
AlexBatya <alex28.12.12@mail.ru>
2022-07-29 23:15:34 +0300

    'correct3'

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-Режим вывода команд на экран (ECHO) включен.
+const button = document.querySelector('button')
+const article = document.querySelector('article')
+
+button.onclick = function(event) {
+    article.classList.add('popa')
+    article.insertAdjacentHTML('afterBegin',`<div class = 'cardPop'>
+    <button class = 'X'>X</button>
+    </div>`)
+    const X =document.querySelector('.X') 
+    const popa = document.querySelector('.popa')
+        X.onclick = function(event) {
+            // popa.querySelector('button').remove()    
+            article.classList.remove('popa')
+
+        }
+}
+
+if(document.querySelector('.popa')) {
+    
+}
