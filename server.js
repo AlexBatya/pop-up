ce9f2e7751fc02f4e0107ca427950b2f3cf35be3
AlexBatya <alex28.12.12@mail.ru>
2022-07-31 14:39:26 +0300

    'qwerrt'

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'js')))
+app.use(express.static(path.join( 'public')))
 
 app.get('/',(req,res) => {
      res.sendFile(`${__dirname}/public/main.html`);
